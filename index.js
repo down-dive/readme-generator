dde93b534638ffadc3dea80aede26c8703522eb2
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-03-30 18:59:48 -0700

    fixing a mess

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs')
 const inquirer = require('inquirer');
+const generatePage = require('./utils/generateMarkdown');
 // const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
@@ -141,70 +142,24 @@ function promtpProject() {
         {
             type: 'list',
             name: 'license',
-            message: 'What license did you use for you project?',
+            message: 'What license did you use for your project?',
             choices: ['Apache', 'Boost', 'Eclipse', 'GNU', 'IBM', 'MIT', 'Mozilla'],
                 }
-    ]).then((res) => {
-        console.log(res)
-        fs.writeFile('./readme.md', generatePage(res.username, res.email, res.title, res.link, res.desctibtion, res.installation, res.usage, res.contrubute, res.tests, res.license), err => {
-            if (err) throw new Error(err);
-        })
+    ])
+}
 
+const makeitWork = (res) =>  {
+    console.log(res)
+    fs.writeFile('./readme.md', generatePage, promtpProject(res.username, res.email, res.title, res.link, res.desctibtion, res.installation, res.usage, res.contrubute, res.tests, res.
+    license), err => {
+        if (err) throw new Error(err);
     })
 
 }
 
-const generatePage = (username, email, title, link, describtion, installation, usage, contribute, tests, license) => {
-    return `
-    # ${title}
-
-    ## Describtion
-
-    ${describtion}
-
-    ## Table of Contents
-
-    * [Installation](#installation) 
-    * [Usage](#usage)
-    * [Contributions](#contributions)
-    * [Testing](#testing)
-    * [Deployed Link](#link)
-    * [Qurestions](#questions)
-    * [License](#license)
-
-    ## Installatioln
-
-    ${installation}
-
-    ## Usage
-
-    ${usage}
-
-    ## Contibutions
-
-    ${contribute}
 
 
-    ## Testing
-    ${tests}
-
-    ## Deployed link
-
-    ${link}
-
-    ## Questions
-
-    Github link: https://github.com/${username}
-    Email: ${email}
-
-    ## License
-
-    License that was used for this project: ${license}
-
-    `
-};
-
-promtpProject()
+makeitWork();
 
 
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -17,4 +17,56 @@ function generateMarkdown(data) {
 `;
 }
 
-module.exports = generateMarkdown;
+
+
+const generatePage = (username, email, title, link, describtion, installation, usage, contribute, tests, license) => {
+  return `
+  # ${title}
+
+  ## Describtion
+
+  ${describtion}
+
+  ## Table of Contents
+
+  * [Installation](#installation) 
+  * [Usage](#usage)
+  * [Contributions](#contributions)
+  * [Testing](#testing)
+  * [Deployed Link](#link)
+  * [Qurestions](#questions)
+  * [License](#license)
+
+  ## Installatioln
+
+  ${installation}
+
+  ## Usage
+
+  ${usage}
+
+  ## Contibutions
+
+  ${contribute}
+
+
+  ## Testing
+  ${tests}
+
+  ## Deployed link
+
+  ${link}
+
+  ## Questions
+
+  Github link: https://github.com/${username}
+  Email: ${email}
+
+  ## License
+
+  License that was used for this project: ${license}
+
+  `
+};
+
+module.exports = generatePage;
